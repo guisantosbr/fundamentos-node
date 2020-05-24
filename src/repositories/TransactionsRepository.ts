ceef924aa968e06cb12b626f365b20740117cf84
Guilherme Almeida <guilherme.almeida@pebmed.com.br>
2020-05-24 17:00:16 -0300

    Criar transação e listar transação.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,46 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    if (this.transactions.length === 0) {
+      return { income: 0, outcome: 0, total: 0 };
+    }
+
+    const transactionByIncome = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const income = transactionByIncome.reduce(
+      (sum: number, currentValue: Transaction) => {
+        return sum + currentValue.value;
+      },
+      0,
+    );
+
+    const transactionByOutcome = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+    const outcome = transactionByOutcome.reduce(
+      (sum: number, currentValue: Transaction) => {
+        return sum + currentValue.value;
+      },
+      0,
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
